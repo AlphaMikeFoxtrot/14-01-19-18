ed25c8f8e2312265228a5bed542b315839f2ff5e
ANONYMOUS <iamcicada3301@gmail.com>
2018-09-18 16:22:08 +0530

    added .gitignore file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+require("dotenv").config();
 
 const eventsRoute = require("./api/routes/events");
 const galleryItemsRoute = require("./api/routes/galleryItems");
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://nasr-admin:nasr-admin-password@nasr-api-v1-yxyku.mongodb.net/test?retryWrites=true");
+mongoose.connect("mongodb+srv://" + process.env.MON_ATL_URME + ":" + process.env.MON_ATL_PSWD + "@nasr-api-v1-yxyku.mongodb.net/test?retryWrites=true");
 
 app.use("/api/v1/events", eventsRoute);
 app.use("/api/v1/galleryItems", galleryItemsRoute);
