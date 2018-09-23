6a8406608a646fc0f61666632a39e2d7e602225a
ANONYMOUS <iamcicada3301@gmail.com>
2018-09-23 23:22:14 +0530

    added push notification service to new event

--- api/routes/events.js
+++ api/routes/events.js
@@ -2,6 +2,10 @@ const epxress = require('express')
 const router = epxress.Router();
 const mongoose = require("mongoose")
 
+const FCM = require('fcm-push');
+const serverKey = process.env.FRBS_SRVR_K;
+const fcm = new FCM(serverKey);
+
 const checkAuth = require("../middleware/checkAuth")
 
 const Event = require("../models/eventModel");
@@ -92,24 +96,48 @@ router.post("/", checkAuth, function (req, res, next) {
             return event.save()
         })
         .then((response) => {
-            console.log(response);
-            res.status(200).json({
-                message: "successfully uploaded new event", 
-                event: {
-                    _id: response._id, 
-                    branch_id: branch_id,
-                    title: response.title, 
-                    body: response.body, 
-                    addedOn: response.addedOn, 
-                    location: response.location, 
-                    eventDate: response.eventDate, 
-                    meta: {
-                        type: "GET", 
-                        description: "Get a list of all the events", 
-                        url: "https://nameless-harbor-15056.herokuapp.com/api/v1/events"
-                    }
+            const message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+                to: '/topics/all',
+                collapse_key: '229626601465',
+
+                notification: {
+                    title: req.body.title,
+                    body: req.body.body
+                },
+
+                data: { //you can send only notification or only data(or include both)
+                    notificationd_id: response._id,
                 }
-            })
+            };
+
+            fcm.send(message, function (err, result) {
+                if (err) {
+                    console.log("Something has gone wrong!");
+                    res.status(500).json({
+                        message: "error sending",
+                        error: err
+                    })
+                } else {
+                    console.log(response);
+                    res.status(200).json({
+                        message: "successfully uploaded new event",
+                        event: {
+                            _id: response._id,
+                            branch_id: branch_id,
+                            title: response.title,
+                            body: response.body,
+                            addedOn: response.addedOn,
+                            location: response.location,
+                            eventDate: response.eventDate,
+                            meta: {
+                                type: "GET",
+                                description: "Get a list of all the events",
+                                url: "https://nameless-harbor-15056.herokuapp.com/api/v1/events"
+                            }
+                        }
+                    })
+                }
+            });
         })
         .catch((error) => { 
             console.log(error);
